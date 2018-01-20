e22a8fcf575d6353ac77f37cab184987bb5d781b
Razdom <afteraz@gmail.com>
2018-01-20 17:24:57 +0200

    add options to zone manager

--- Sevrer/Server/zonesManager.js
+++ Sevrer/Server/zonesManager.js
@@ -1,7 +1,33 @@
 var zones = [];
+var lastZoneID = 0;
 
-function zonesMnager(){
+function zonesMnager() {
 
 }
 
+function createNewZone(name, maxPlayers, defultJoinRoom) {
+  var zone = new zoneObj(lastZoneID++, name, maxPlayers, defultJoinRoom);
+  zones.push(zone);
+  return zone;
+}
+
+function getZoneByID(id) {
+  for (var z in zone)
+    if (zone[z].id == id)
+      return zone[z];
+  return null;
+}
+
+function getZoneByName(name) {
+  for (var z in zone)
+    if (zone[z].name == name)
+      return zone[z];
+  return null;
+}
+
+
+
 module.exports = zonesMnager;
+module.exports.createNewZone = createNewZone;
+module.exports.getZoneByID = getZoneByID;
+module.exports.getZoneByName = getZoneByName;
