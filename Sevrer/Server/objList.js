3ef316ad858e2b07338713dcdf9bf52cf57f12c4
Razdom <afteraz@gmail.com>
2018-01-20 17:24:33 +0200

    add Object List file.

---
+++ Sevrer/Server/objList.js
@@ -0,0 +1,3 @@
+global.zoneObj = require('./obj/zone.js');
+global.roomObj = require('./obj/room.js');
+global.playerObj = require('./obj/player.js');
--- Sevrer/index.js
+++ Sevrer/index.js
@@ -1,8 +1,9 @@
 var net = require('net');
+var objList = require('./Server/objList.js');
 var configManager = require('./Server/configManager.js')();
 
 function luanchServer() {
-  
+
 }
 
 luanchServer();
