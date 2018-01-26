d78b2c999adba8ea329bc301880cc68936f49b9f
Razdom <afteraz@gmail.com>
2018-01-26 08:35:00 +0200

    Fix The zones Manager

--- Sevrer/Server/zonesManager.js
+++ Sevrer/Server/zonesManager.js
@@ -1,8 +1,8 @@
 var zones = [];
-var lastZoneID = 0;
+var lastID = 0;
 
 function createNewZone(name, maxPlayers, defultJoinRoom, canJoin) {
-  var zone = new zoneObj(lastZoneID++, name, maxPlayers, defultJoinRoom, canJoin);
+  var zone = new zoneObj(lastID++, name, maxPlayers, defultJoinRoom, canJoin);
   zones.push(zone);
   return zone;
 }
@@ -12,16 +12,16 @@ function getZones() {
 }
 
 function getZoneByID(id) {
-  for (var z in zone)
-    if (zone[z].id == id)
-      return zone[z];
+  for (var z in zones)
+    if (zones[z].id == id)
+      return zones[z];
   return null;
 }
 
 function getZoneByName(name) {
-  for (var z in zone)
-    if (zone[z].name == name)
-      return zone[z];
+  for (var z in zones)
+    if (zones[z].name == name)
+      return zones[z];
   return null;
 }
 
@@ -36,4 +36,4 @@ module.exports.getZones = getZones;
 module.exports.createNewZone = createNewZone;
 module.exports.getZoneByID = getZoneByID;
 module.exports.getZoneByName = getZoneByName;
-module.exports.removeZone = getZoneByName;
+module.exports.removeZone = removeZone;
