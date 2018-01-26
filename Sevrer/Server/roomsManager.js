716a89d3db2765e44b008c0b1bbea43710a445ff
Razdom <afteraz@gmail.com>
2018-01-26 08:34:50 +0200

    Create The Room Manager

--- Sevrer/Server/roomsManager.js
+++ Sevrer/Server/roomsManager.js
@@ -1,7 +1,36 @@
 var rooms = [];
+var lastID = 0;
 
-function roomsMnager() {
-
+function createNewRoom(name, maxPlayers, canJoin) {
+  var room = new room(lastID++, name, maxPlayers, canJoin);
+  rooms.push(room);
 }
 
-module.exports = roomsMnager;
+function getRooms() {
+  return rooms;
+}
+
+function getRoomByID(id) {
+  for (var r in rooms)
+    if (rooms[r].id == id)
+      return rooms[r];
+  return null;
+}
+
+function getRoomByName(name) {
+  for (var r in rooms)
+    if (rooms[r].name == name)
+      return rooms[r];
+  return null;
+}
+
+function removeRoom(room) {
+  if (room != null && rooms.indexOf(room) > -1)
+    rooms.splice(rooms.indexOf(room), 1);
+}
+
+module.exports.getRooms = getRooms;
+module.exports.createNewRoom = createNewRoom;
+module.exports.getRoomByID = getRoomByID;
+module.exports.getRoomByName = getRoomByName;
+module.exports.removeRoom = removeRoom;
