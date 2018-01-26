a510c7f98d50c22d26969b648632dac56139e158
Razdom <afteraz@gmail.com>
2018-01-26 08:49:52 +0200

    * ADD Socket Parameter to the Player Obj * Remove some functions

--- Sevrer/Server/obj/player.js
+++ Sevrer/Server/obj/player.js
@@ -1,22 +1,8 @@
-function player(id, username, zone, isAdministrator) {
+function player(id, name, socket, isAdministrator) {
   this.id = id;
   this.name = name;
-  this.zone = zone;
+  this.socket = socket;
   this.isAdministrator = isAdministrator;
-  this.rooms = [];
-  this.players = [];
-}
-
-function addToRoom(room) {
-  rooms.push(room);
-  room.addPlayer(this);
-}
-
-function removeFromRoom(room) {
-  rooms.splice(rooms.indexOf(room), -1);
-  room.removePlayer(this);
 }
 
 module.exports = player;
-player.prototype.addToRoom = addToRoom;
-player.prototype.removePlayer = removePlay;
